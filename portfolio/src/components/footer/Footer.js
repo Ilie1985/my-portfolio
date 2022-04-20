9f562e14a3bdba7824daa65bd8e20f67c81501f3
Marian Ilie <ilie.marian32@yahoo.com>
2022-04-20 15:45:44 +0100

    footer

--- portfolio/src/components/footer/Footer.js
+++ portfolio/src/components/footer/Footer.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { FiInstagram } from "react-icons/fi";
+import { RiLinkedinBoxLine } from "react-icons/ri";
+import "./footer.css";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer_logo'></a>
-    </footer>
-  )
-}
+      <a href="#" className="footer_logo">
+        Marian Ilie
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contacts">Home</a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="footer_socials">
+        <a href="https://facebook.com">
+          <RiFacebookCircleLine />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://linkedin.com">
+          <RiLinkedinBoxLine />
+        </a>
+      </div>
+      <div className="footer_copyright">
+        <small>&copy; Marian`s Portfolio 2022 </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
