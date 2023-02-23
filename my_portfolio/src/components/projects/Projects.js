a8dc172ceb46123e3f3617002e0f30a45efd3bb5
Marian Ilie <ilie.marian32@yahoo.com>
2023-02-23 10:33:31 +0000

    more projects added

--- my_portfolio/src/components/projects/Projects.js
+++ my_portfolio/src/components/projects/Projects.js
@@ -1,6 +1,10 @@
 import React from "react";
 import img1 from "../../media/img1.jpg";
 import img2 from "../../media/img2.jpg";
+import img3 from "../../media/img3.jpg"
+import img4 from "../../media/img4.jpg"
+import img5 from "../../media/img5.jpg"
+import img6 from "../../media/img6.jpg"
 import "./projects.css";
 
 const data = [
@@ -18,6 +22,34 @@ const data = [
     title: "RPS Project",
     github: "https://github.com/Ilie1985/new_rps/blob/main/new_rps/src/App.js",
     demo: "https://rps-application.netlify.app",
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "Github-User Search",
+    github: "hhttps://github.com/Ilie1985/search_github_users_reactjs",
+    demo: "https://githubuser-search-md.netlify.app/",
+  },
+  {
+    id: 4,
+    image: img4,
+    title: "Jobrecord",
+    github: "https://github.com/Ilie1985/job_finder_reactjs",
+    demo: "https://jobrecord.netlify.app/",
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "Comfysloth",
+    github: "https://github.com/Ilie1985/comfy_sloth_project",
+    demo: "https://comfyslooth.netlify.app/",
+  },
+  {
+    id: 6,
+    image: img6,
+    title: "Cocktails",
+    github: "https://github.com/Ilie1985/cocktails_reactjs",
+    demo: "https://cocktailscd.netlify.app/",
   },
 ];
 
