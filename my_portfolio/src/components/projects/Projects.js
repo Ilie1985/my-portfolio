ef1fe89aaf0685222e0cdbd5ebddd1b84aa46f91
Marian Ilie <ilie.marian32@yahoo.com>
2023-03-23 14:19:07 +0000

    change the order of displaying the apps

--- my_portfolio/src/components/projects/Projects.js
+++ my_portfolio/src/components/projects/Projects.js
@@ -12,37 +12,12 @@ import "./projects.css";
 
 const data = [
   {
-    id: 1,
-    image: img1,
-    title: "Space Project",
-    github:
-      "https://github.com/SchoolOfCode/final-project-front-end-zoomage-daydream",
-    demo: "https://spaces-application.netlify.app",
-    description:" This project is created by team of six people following the Agile methodology as a final project of School of Code Bootcamp .Though there is no commercial business behind this app, the platform itself is a functional app bringing the aim to improve the environment in our community by working with area businesses, community leaders and our neighbors to create a clean and safe place to work. Used React.js, Node.js, Heroku, Postman, PostgreSQL ,Cypruss for testing, TDD,Auth0, Netlify, Cloudinary.com"
-  },
-  {
-    id: 2,
-    image: img2,
-    title: "RPS Project",
-    github: "https://github.com/Ilie1985/new_rps/blob/main/new_rps/src/App.js",
-    demo: "https://rps-application.netlify.app",
-    description:"React js project  with useState,useEffect"
-  },
-  {
-    id: 3,
-    image: img3,
-    title: "Github-User Search",
-    github: "hhttps://github.com/Ilie1985/search_github_users_reactjs",
-    demo: "https://githubuser-search-md.netlify.app/",
-    description:"React js project ,used React Router 6,useState,useEffect,useContext, auth0"
-  },
-  {
     id: 4,
     image: img4,
     title: "Jobrecord",
     github: "https://github.com/Ilie1985/job_finder_reactjs",
     demo: "https://jobrecord.netlify.app/",
-    description:"React js project ,used React Router 6,useState,react-redux "
+    description: "React js project ,used React Router 6,useState,react-redux ",
   },
   {
     id: 5,
@@ -50,7 +25,8 @@ const data = [
     title: "Comfysloth",
     github: "https://github.com/Ilie1985/comfy_sloth_project",
     demo: "https://comfyslooth.netlify.app/",
-    description:"React js project ,used React Router 6,useState,useEffect, UseContext,useReducer hooks, auth0 for authentication "
+    description:
+      "React js project ,used React Router 6,useState,useEffect, UseContext,useReducer hooks, auth0 for authentication ",
   },
   {
     id: 6,
@@ -58,7 +34,8 @@ const data = [
     title: "Cocktails",
     github: "https://github.com/Ilie1985/cocktails_reactjs",
     demo: "https://cocktailscd.netlify.app/",
-    description:"React js project ,used React Router 6,useState,useEffect and UseContext hooks"
+    description:
+      "React js project ,used React Router 6,useState,useEffect and UseContext hooks",
   },
   {
     id: 7,
@@ -66,7 +43,8 @@ const data = [
     title: "Realtor",
     github: "https://github.com/Ilie1985/realtor_reactjs",
     demo: "https://realtor-reactjs-rho.vercel.app/",
-    description:"React js project ,used Firebase auth for complete authentication,Firebase Firestore to store and fetch the data,tailwind css 3,CRUD operations using Firebase Firestore,added a map to the page using leaflet and react leaflet packages,used Google geolocation API to convert address to latitude and longitude,deployed to Vercel"
+    description:
+      "React js project ,used Firebase auth for complete authentication,Firebase Firestore to store and fetch the data,tailwind css 3,CRUD operations using Firebase Firestore,added a map to the page using leaflet and react leaflet packages,used Google geolocation API to convert address to latitude and longitude,deployed to Vercel",
   },
   {
     id: 8,
@@ -74,7 +52,8 @@ const data = [
     title: "IMDb Clone",
     github: "https://github.com/Ilie1985/imdb_react_nextjs",
     demo: "https://imdb-react-nextjs.vercel.app/",
-    description:"IMDb clone project using Next.js 13 and Tailwind CSS, fetched data from TMDB API and deployed to Vercel"
+    description:
+      "IMDb clone project using Next.js 13 and Tailwind CSS, fetched data from TMDB API and deployed to Vercel",
   },
   {
     id: 9,
@@ -82,7 +61,35 @@ const data = [
     title: "Google Clone",
     github: "https://github.com/Ilie1985/Google_clone_nextjs13",
     demo: "https://google-clone-nextjs13.vercel.app/",
-    description:"Google clone project using Next.js 13 and Tailwind CSS, fetched data from Google search API and deployed to Vercel"
+    description:
+      "Google clone project using Next.js 13 and Tailwind CSS, fetched data from Google search API and deployed to Vercel",
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "RPS Project",
+    github: "https://github.com/Ilie1985/new_rps/blob/main/new_rps/src/App.js",
+    demo: "https://rps-application.netlify.app",
+    description: "React js project  with useState,useEffect",
+  },
+  {
+    id: 1,
+    image: img1,
+    title: "Space Project",
+    github:
+      "https://github.com/SchoolOfCode/final-project-front-end-zoomage-daydream",
+    demo: "https://spaces-application.netlify.app",
+    description:
+      " This project is created by team of six people following the Agile methodology as a final project of School of Code Bootcamp .Though there is no commercial business behind this app, the platform itself is a functional app bringing the aim to improve the environment in our community by working with area businesses, community leaders and our neighbors to create a clean and safe place to work. Used React.js, Node.js, Heroku, Postman, PostgreSQL ,Cypruss for testing, TDD,Auth0, Netlify, Cloudinary.com",
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "Github-User Search",
+    github: "hhttps://github.com/Ilie1985/search_github_users_reactjs",
+    demo: "https://githubuser-search-md.netlify.app/",
+    description:
+      "React js project ,used React Router 6,useState,useEffect,useContext, auth0",
   },
 ];
 
@@ -91,7 +98,7 @@ const Projects = () => {
     <section id="projects">
       <h3>My recent work</h3>
       <div className="projects_container">
-        {data.map(({ id, image, title, github, demo,description}) => {
+        {data.map(({ id, image, title, github, demo, description }) => {
           return (
             <article key={id} className="projects_item">
               <div className="projects_item-img">
@@ -120,7 +127,7 @@ const Projects = () => {
                   See the app
                 </a>
               </div>
-                <p>{description}</p>
+              <p>{description}</p>
             </article>
           );
         })}
