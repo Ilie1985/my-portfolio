0f817cc1fb3c639375ed780bacce5f45ccf88fb4
Marian Ilie <ilie.marian32@yahoo.com>
2023-05-15 11:57:17 +0100

    implemented features

--- my_portfolio/src/components/projects/Projects.js
+++ my_portfolio/src/components/projects/Projects.js
@@ -82,7 +82,7 @@ const data = [
     github: "https://github.com/Ilie1985/Google_clone_nextjs13",
     demo: "https://google-clone-nextjs13.vercel.app/",
     description:
-      "Google clone project using Next.js 13 and Tailwind CSS, fetched data from Google search API and deployed to Vercel, Sign in feature not implemented yet",
+      "Google clone project using Next.js 13 and Tailwind CSS, fetched data from Google search API and deployed to Vercel, Sign in feature not implemented yet,gmail,google search, feeling lucky ,google images features implemented",
   },
   {
     id: 2,
